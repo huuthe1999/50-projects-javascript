bc3f23e3f71c91e489479fa5b9133e33172eb387
huuthe1999 <huuthe128@gmail.com>
2022-05-14 15:09:58 +0700

    Deploy project

--- day03-rotating_navigation_animation/script.js
+++ day03-rotating_navigation_animation/script.js
@@ -4,12 +4,21 @@ const container = document.querySelector('.container');
 
 openBtn.addEventListener('click', () => {
 	container.classList.add('toggle');
-	document.body.classList.add('stop-scrolling');
+	// document.body.classList.add('stop-scrolling');
 });
 
 closeBtn.addEventListener('click', () => {
 	container.classList.remove('toggle');
-	document.body.classList.remove('stop-scrolling');
+	// document.body.classList.remove('stop-scrolling');
 	// document.querySelector('.container-circle').style.transform =
 	// 	'rotate(' + 0 + 'deg)';
+});
+
+// Event on navigation
+
+const liList = document.querySelectorAll('ul li');
+liList.forEach(li => {
+	li.addEventListener('click', () => {
+		container.classList.remove('toggle');
+	});
 });
